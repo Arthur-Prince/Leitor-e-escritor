26b425e3583fff27ddf8ab7264b0b8cccdc4c62f
Arthur-Prince <56482309+Arthur-Prince@users.noreply.github.com>
2022-11-11 17:32:25 -0300

    Add files via upload

---
+++ src/app/LeitorEscritor.java
@@ -0,0 +1,68 @@
+package app;
+
+class LeitorEscritor {
+	int leitoresAtivos = 0;
+	int escritoresAtivos = 0;
+	boolean escritorPresente = false;
+
+	/*verifica se pode escrever*/
+	boolean condicaoEscrita() {
+		return leitoresAtivos == 0 && !escritorPresente;
+	}
+
+	/*verifica se pode ler*/
+	boolean condicaoLeitura() {
+		return escritoresAtivos <= 0 && !escritorPresente;
+	}
+	
+
+	/*enquanto não puder ler, espera. Depois aumenta o número de leitores ativos*/
+	synchronized void comecarLeitura() {
+		while (!condicaoLeitura()) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		leitoresAtivos++;
+	}
+	
+	/*tira um leitor ativo*/
+	synchronized void pararLeitura() {
+		leitoresAtivos--;
+		notifyAll();
+	}
+
+	/*enquanto não puder escrever, espera. Depois coloca um escritor como presente*/
+	synchronized void comecarEscrita() {
+		while (!condicaoEscrita()) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		escritorPresente = true;
+	}
+	
+	/*coloca-se presente e espera todos os leitores pararem de ler*/
+	synchronized void comecarEscrita2() {
+		escritoresAtivos++;
+		while (!condicaoEscrita()) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		escritorPresente = true;
+	}
+
+	/*tira o escritor presente*/
+	synchronized void pararEscrita() {
+		escritoresAtivos--;
+		escritorPresente = false;
+		notifyAll();
+	}
+}
